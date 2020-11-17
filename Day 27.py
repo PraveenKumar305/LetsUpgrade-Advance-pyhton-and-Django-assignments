8ce0fb8f7b65e2f3bc2c84a2a6b02d5ef5219061
Praveen Kumar <71426969+heypraveen@users.noreply.github.com>
2020-11-17 14:12:45 +0530

    Create Day 27.py

---
+++ Day 27.py
@@ -0,0 +1,34 @@
+import datetime
+main_list = list(range(100000000))
+t1 = datetime.datetime.now()
+list1 = (each**2 for each in main_list)
+for each in list1:
+  each
+t2 = datetime.datetime.now()
+print (t2-t1)
+
+OUTPUT: 0:00:11.711889
+*******************************
+
+
+import datetime
+main_list = list(range(100000000))
+t1 = datetime.datetime.now()
+list1 = (each**2 for each in main_list)
+for each in list1:
+  each
+t2 = datetime.datetime.now()
+print (t2-t1)
+
+OUTPUT: 0:00:11.161044
+*********************************
+
+main_list = list(range(100000000))
+t1 = datetime.datetime.now()
+list1 = (each**2 for each in main_list)
+for each in list1:
+  each
+t2 = datetime.datetime.now()
+print (t2-t1)
+
+OUTPUT: 0:00:11.547689
